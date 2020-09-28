3dd24067a8bcd6165049ec8d38d302d946aea417
jhomathy <verifiedJhomathy@gmail.com>
2020-09-28 02:06:41 -0700

    testing on mobile

---
+++ ytgAST.hs
@@ -0,0 +1,12 @@
+-- YouTube Title Grammar Abstract Syntax Tree
+
+-- TitleMade ::= [Element]
+-- Element   ::= 0
+
+-- Keyword is a String
+-- Avoid   is a [String]
+
+-- also should take a random IO () result
+scramble :: String -> String
+scramble name = name
+
